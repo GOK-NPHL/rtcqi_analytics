17756eeb3701ca0ca84b30c789ae090fdb8f106c
Ndiithi <duncanndiithi@yahoo.com>
2021-04-18 11:32:21 +0300

    echarts dependency

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,6 +23,8 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+
+var EchartsForReact=require('echarts-for-react');
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
@@ -39,3 +41,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+// window.echarts-for-react   = require('echarts-for-react');
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,4 @@ const mix = require('laravel-mix');
 mix.react('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .postCss('resources/css/sb-admin-2.css', 'public/css');
+
