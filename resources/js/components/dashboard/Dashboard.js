084f8ed8d05f3f4b32a55e90c777bf7d3ff97fb0
Ndiithi <duncanndiithi@yahoo.com>
2021-04-18 13:07:22 +0300

    bar chart

--- resources/js/components/dashboard/Dashboard.js
+++ resources/js/components/dashboard/Dashboard.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import StatsLabel from '../stats/StatsLabel';
+import LineGraph from '../charts/LineGraph';
+import RTCard from '../utils/RTCard';
+
 
 
 class Dashboard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataset1: {
+                dimensions: ['indicator', 'Baseline(Round 13)', 'Y1_Q4(round 14)', 'Y2_Q1(round 15)', 'Y2_Q2(round 16)'],
+
+                source: [
+                    { indicator: 'Providers Enrolled', 'Baseline(Round 13)': 43.3, 'Y1_Q4(round 14)': 85.8, 'Y2_Q1(round 15)': 93.7, 'Y2_Q2(round 16)': 73.7 },
+                    { indicator: 'Providers With PT Results', 'Baseline(Round 13)': 83.1, 'Y1_Q4(round 14)': 73.4, 'Y2_Q1(round 15)': 55.1, 'Y2_Q2(round 16)': 78.7 },
+                    { indicator: 'Providers With Satisfactory Results', 'Baseline(Round 13)': 86.4, 'Y1_Q4(round 14)': 65.2, 'Y2_Q1(round 15)': 82.5, 'Y2_Q2(round 16)': 89.7 },
+                    { indicator: 'Providers That Received Corrective', 'Baseline(Round 13)': 72.4, 'Y1_Q4(round 14)': 53.9, 'Y2_Q1(round 15)': 39.1, 'Y2_Q2(round 16)': 47.7 }
+                ]
+
+            },
+            series1: [
+                { type: 'bar' },
+                { type: 'bar' },
+                { type: 'bar' },
+                { type: 'bar' }
+            ]
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -61,7 +88,13 @@ class Dashboard extends React.Component {
 
                 </div>
 
-
+                <div className="row">
+                    <div className="col-xl-6 col-lg-6">
+                        <RTCard header='Providers Statstics'>
+                            <LineGraph dataset={this.state.dataset1} series={this.state.series1} />
+                        </RTCard>
+                    </div>
+                </div>
 
             </React.Fragment>
         );
