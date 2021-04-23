b1f685376296dfafa8992764dbbbaaa500952583
Ndiithi <duncanndiithi@yahoo.com>
2021-04-23 17:17:43 +0300

    form submissions migration table

---
+++ database/migrations/2021_04_23_133315_create_form_submissions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_submissions', function (Blueprint $table) {
+            $table->id();
+            $table->string("lastest_submission_date");
+            $table->string("form_id");
+            $table->bigIncrements('org_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_submissions');
+    }
+}
