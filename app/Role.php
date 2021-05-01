63f19f123c04837a8f1e710539b08ef90b1d6f2b
duncan <duncanndiithi@yahoo.com>
2021-05-01 17:53:09 +0300

    add relationship to role

--- app/Role.php
+++ app/Role.php
@@ -5,10 +5,16 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
-{   
+{
+    protected $hidden = ['updated_at'];
 
     public function authorities()
     {
         return $this->belongsToMany('App\Authority');
+    }
+
+    public function editor()
+    {
+        return $this->hasOne('App\User','editor_id');
     }
 }
