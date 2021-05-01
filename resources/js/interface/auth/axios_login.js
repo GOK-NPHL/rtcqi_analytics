77465511f3f0b449f2dcee8ea5a62563e2b74501
duncan <duncanndiithi@yahoo.com>
2021-05-01 14:27:38 +0300

    remove alert func

--- resources/js/interface/auth/axios_login.js
+++ resources/js/interface/auth/axios_login.js
@@ -2,7 +2,6 @@ $('#login_form').one('submit', function (e) {
     e.preventDefault();
    
     let res =axios.get('/sanctum/csrf-cookie').then(response => {
-        alert("hold up");
         $(this).submit();
     });
     console.log(res);
