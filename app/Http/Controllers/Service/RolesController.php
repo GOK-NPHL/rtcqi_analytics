4bcd7df3826acd80ce65a2fb4472dc950ab3a925
duncan <duncanndiithi@yahoo.com>
2021-05-05 22:41:52 +0300

    return roles

--- app/Http/Controllers/Service/RolesController.php
+++ app/Http/Controllers/Service/RolesController.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Role;
-use Illuminate\Support\Facades\Log;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class RolesController extends Controller
 {
@@ -33,18 +35,29 @@ class RolesController extends Controller
 
     public function getRoles()
     {
-        $roles = DB::table('roles')
-            ->join('users', 'editor_id', '=', 'users.id')
-            ->get();
+        // $roles = DB::table('roles')
+        //     ->join('users', 'editor_id', '=', 'users.id')
+        //     ->get();
+        $roles = Role::select(
+            "users.name as editor",
+            "roles.name as role_name",
+            "roles.updated_at",
+            "roles.id as role_id",
+        )->join('users', 'editor_id', '=', 'users.id')->get();
         return $roles;
     }
 
     public function createRole(Request $request)
     {
-        $user = Auth::user();
-        $role = new Role(['name' => $request->name]);
-        $role->editor()->associate($user);
-        $role->save();
-        $role->authorities()->sync($request->authoritiesSelected, false);
+        try {
+            $user = Auth::user();
+            $role = new Role(['name' => $request->name]);
+            $role->editor()->associate($user);
+            $role->save();
+            $role->authorities()->sync($request->authoritiesSelected, false);
+        } catch (Exception $ex) {
+
+            return ['Error' => '500', 'Message' => 'Could not save role ' . $ex->getCode()];
+        }
     }
 }
