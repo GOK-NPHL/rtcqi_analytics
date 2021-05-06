a93d7ec169e552ab4c655689bb49496c6db20f49
duncan <duncanndiithi@yahoo.com>
2021-05-06 03:49:24 +0300

    edit user controller

--- app/Http/Controllers/Service/RolesController.php
+++ app/Http/Controllers/Service/RolesController.php
@@ -10,6 +10,8 @@ use App\Role;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 
 class RolesController extends Controller
 {
@@ -40,8 +42,39 @@ class RolesController extends Controller
             "roles.name as role_name",
             "roles.updated_at as updated_at",
             "roles.id as role_id",
-        )->join('users', 'editor_id', '=', 'users.id')->get();
-        return $roles;
+            "authorities.name as authority_name",
+            "authorities.id as authority_id",
+            "authorities.group as authority_group"
+        )->join('users', 'editor_id', '=', 'users.id')
+            ->join('authority_role', 'roles.id', '=', 'authority_role.role_id')
+            ->join('authorities', 'authorities.id', '=', 'authority_role.authority_id')
+            ->get();
+        $roleIds = array();
+        $payload = array();
+
+        foreach ($roles as $role) {
+            if (in_array($role->role_id, $roleIds)) {
+                $auths = $payload[$role->role_id]["authorities"];
+                if (array_key_exists($role->authority_group, $auths)) {
+                    $payload[$role->role_id]["authorities"][$role->authority_group][] = $role->authority_id;
+                } else {
+                    $payload[$role->role_id]["authorities"][$role->authority_group] = [$role->authority_id];
+                }
+                [$role->authority_id] = $role->authority_name;
+            } else {
+                $roleEntry =
+                    $payload[$role->role_id] = [
+                        "editor" => $role->editor,
+                        "role_name" => $role->role_name,
+                        "updated_at" => \Carbon\Carbon::parse($role->updated_at)->format('Y-m-d H:i:s'),
+                        "role_id" => $role->role_id,
+                        "authorities" => [$role->authority_group => [$role->authority_id]]
+
+                    ];
+                $roleIds[] = $role->role_id;
+            }
+        }
+        return  $payload;
     }
 
     public function createRole(Request $request)
@@ -52,9 +85,10 @@ class RolesController extends Controller
             $role->editor()->associate($user);
             $role->save();
             $role->authorities()->sync($request->authoritiesSelected, false);
+            return response()->json(['Message' => 'Created successfully'], 200);
         } catch (Exception $ex) {
 
-            return ['Error' => '500', 'Message' => 'Could not save role ' . $ex->getCode()];
+            return ['Error' => '500', 'Message' => 'Could not save role ' . $ex->getMessage()];
         }
     }
 
@@ -65,7 +99,27 @@ class RolesController extends Controller
             $role->delete();
             return response()->json(['Message' => 'Deleted successfully'], 200);
         } catch (Exception $ex) {
-            return response()->json(['Message' => 'Delete failed.  Error code' . $ex->getCode()], 500);
+            return response()->json(['Message' => 'Delete failed.  Error code' . $ex->getMessage()], 500);
+        }
+    }
+
+    public function updateRole(Request $request)
+    {
+        try {
+            $user = Auth::user();
+            $role = Role::find($request->role_id);
+            Log::debug("role name db " . $role->name);
+            Log::debug("role name req " . $request->name);
+            $role->name = $request->name;
+            $role->save();
+            Log:info("role name updated");
+            $role->editor()->associate($user);
+            $role->save();
+            $role->authorities()->sync($request->authoritiesSelected);
+            return response()->json(['Message' => 'Updated successfully'], 200);
+        } catch (Exception $ex) {
+
+            return ['Error' => '500', 'Message' => 'Could not save role: ' . $ex->getMessage()];
         }
     }
 }
