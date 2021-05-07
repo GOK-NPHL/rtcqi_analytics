ec5e50734ed25a74020c075e8e79a1d1e753bff8
duncan <duncanndiithi@yahoo.com>
2021-05-07 13:40:21 +0300

    define org unit structure

--- resources/js/components/system/org-unit/OrgUnits.js
+++ resources/js/components/system/org-unit/OrgUnits.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TreeView from '../../utils/TreeView';
 
-class Orgunit extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
+function Orgunit() {
 
-        }
-    }
+    const imgStyle = {
+        width: "100%"
+    };
 
-    render() {
-        const imgStyle = {
-            width: "100%"
-        };
+    const rowStle = {
+        marginBottom: "5px"
+    };
 
-        const rowStle = {
-            marginBottom: "5px"
-        };
 
-        return (
-            <React.Fragment>
+    return <React.Fragment>
 
-                {/* Page Heading */}
-                <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 className="h4 mb-0 text-gray-500">Organisation Unit Management</h1>
-                    <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                        className="fas fa-sitemap fa-sm text-white-50"></i> Create Organisation Unit</a>
-                </div>
+        {/* Page Heading */}
+        <div className="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 className="h4 mb-0 text-gray-500">Organisation Unit Management</h1>
+            <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                className="fas fa-sitemap fa-sm text-white-50"></i> Create Organisation Unit</a>
+        </div>
+        <TreeView />
 
-            </React.Fragment>
-        );
-    }
-
+    </React.Fragment>
 }
 
 export default Orgunit;
 
 if (document.getElementById('orgunits')) {
-    ReactDOM.render(<Orgunit/>, document.getElementById('orgunits'));
+    ReactDOM.render(<Orgunit />, document.getElementById('orgunits'));
 }
