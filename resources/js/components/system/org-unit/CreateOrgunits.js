9e8d31cdbb852895179ecd1956f0f92b8da96801
duncan <duncanndiithi@yahoo.com>
2021-05-09 11:04:51 +0300

    create orgunit component

--- resources/js/components/system/org-unit/CreateOrgunits.js
+++ resources/js/components/system/org-unit/CreateOrgunits.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FetchAuthorities, SaveRole, UpdateRole } from '../../utils/Helpers';
 import DualListBox from 'react-dual-listbox';
-
+import XLSX from "xlsx";
+import SheetSelect from './SheetSelect';
+import LevelSelect from './LevelSelect';
 
 class OrgunitCreate extends React.Component {
 
@@ -10,20 +12,68 @@ class OrgunitCreate extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: [],
+            fileName: "Choose orgunit excel",
+            sheetWithOrgs: '',
+            workbook: [],
         };
+        this.handleFile = this.handleFile.bind(this);
+        this.setSheetWithOrgs = this.setSheetWithOrgs.bind(this);
+
     }
 
     componentDidMount() {
 
     }
 
+    handleFile(e) {
+
+        var files = e.target.files, f = files[0];
+        console.log(files);
+        this.setState({ fileName: files[0].name });
+        var reader = new FileReader();
+        reader.onload = (e) => {
+            var data = new Uint8Array(e.target.result);
+            var workbook = XLSX.read(data, { type: 'array' });
+            console.log(workbook);
+            this.setState({
+                workbook: workbook
+            });
+        };
+        reader.readAsArrayBuffer(f);
+    }
+
+    setSheetWithOrgs(event) {
+        this.setState({
+            sheetWithOrgs: event.target.value,
+        });
+    }
 
     render() {
 
         return (
             <React.Fragment>
-                create orgs
+                <div className="row">
+                    <div className="col-sm-4">
+                        <div className="input-group mb-3">
+                            <div className="custom-file">
+                                <input
+                                    onChange={() => this.handleFile(event)}
+                                    type="file"
+                                    className="custom-file-input"
+                                    accept=".xls,.xlsx" id="inputGroupFile01"
+                                    aria-describedby="inputGroupFileAddon01" />
+                                <label className="custom-file-label" htmlFor="inputGroupFile01">{this.state.fileName}</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <SheetSelect workbook={this.state.workbook} setSheetWithOrgs={this.setSheetWithOrgs} />
+                <hr />
+                <LevelSelect workbook={this.state.workbook} sheetWithOrgs={this.state.sheetWithOrgs} />
+
+
+
             </React.Fragment>
         );
     }
--- resources/js/components/system/org-unit/OrgUnits.js
+++ resources/js/components/system/org-unit/OrgUnits.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import TreeView from '../../utils/TreeView';
+import OrgunitCreate from './CreateOrgunits';
+import DataTable from "react-data-table-component";
+import movies from "./movies";
+
 
 
 function Orgunit() {
+
+    const [showOrgunitLanding, setShowOrgunitLanding] = useState(true);
+
 
     const imgStyle = {
         width: "100%"
@@ -13,17 +20,60 @@ function Orgunit() {
         marginBottom: "5px"
     };
 
+    const columns = [
+        {
+            name: "Title",
+            selector: "title",
+            sortable: true
+        },
+        {
+            name: "Directior",
+            selector: "director",
+            sortable: true
+        },
+        {
+            name: "Runtime (m)",
+            selector: "runtime",
+            sortable: true,
+            right: true
+        }
+    ];
+
+    let pageContent = '';
+
+    if (showOrgunitLanding) {
+        pageContent = <React.Fragment>
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 className="h4 mb-0 text-gray-500">Organisation Unit Management</h1>
+                <a href="#" onClick={() => setShowOrgunitLanding(false)} className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                    className="fas fa-sitemap fa-sm text-white-50"></i> Create Organisation Unit</a>
+            </div>
+            <div className="row">
+                <div className="col-sm-3">
+                    <TreeView />
+                </div>
+                <div className="col-sm-9">
+                    <DataTable
+                        title="Movies"
+                        columns={columns}
+                        data={movies}
+                        defaultSortFieldId={1}
+                        pagination
+                        selectableRows
+                    />
+                </div>
+            </div>
+
+        </React.Fragment>
+
+    } else {
+        pageContent = <OrgunitCreate
+            toggleDisplay={setShowOrgunitLanding}
+        />;
+    }
 
     return <React.Fragment>
-
-        {/* Page Heading */}
-        <div className="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 className="h4 mb-0 text-gray-500">Organisation Unit Management</h1>
-            <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                className="fas fa-sitemap fa-sm text-white-50"></i> Create Organisation Unit</a>
-        </div>
-        <TreeView />
-
+        {pageContent}
     </React.Fragment>
 }
 
