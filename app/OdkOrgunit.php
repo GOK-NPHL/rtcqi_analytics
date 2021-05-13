fdc47819baef2fae7ab6e02d8e55dc38822fda42
duncan <duncanndiithi@yahoo.com>
2021-05-13 21:24:27 +0300

    format date updated column

--- app/OdkOrgunit.php
+++ app/OdkOrgunit.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class OdkOrgunit extends Model
 {
+    protected $casts = [
+        'updated_at' => 'datetime:Y-m-d',
+    ];
     protected $table = 'odk_orgunit_maps';
     protected $fillable = ['org_unit_id', 'odk_orgunit_maps', 'level', 'parent_id', 'odk_unit_name'];
 }
