ac93055052e5e1702646e03bf21647eb369e95c4
duncan <duncanndiithi@yahoo.com>
2021-05-17 20:43:00 +0300

    remove data table, add custom table

--- resources/js/components/system/org-unit/OrgUnits.js
+++ resources/js/components/system/org-unit/OrgUnits.js
@@ -47,10 +47,60 @@ function developOrgStructure(orunitData) {
     });
 }
 
+function updateOrg(org) {
+    console.log(org);
+}
+
+function editOrg(org, setOrgToEdit) {
+    window.$('#editOrgModal').modal();
+    setOrgToEdit(org);
+}
+
+function deleteOrg(orgId) {
+    console.log(orgId);
+}
+
+
+function createOrgunitTable(tableData, setOrgToEdit) {
+    var tableRows = [];
+    if (tableData.length == 0) {
+        tableRows.push(<tr key={1}>
+            <td>1</td>
+            <td colSpan="4" style={{ textAlign: 'center' }}>No Roles Defined</td>
+        </tr>);
+    } else {
+        tableData.payload[0].map((value, index) => {
+            index = index + 1;
+            tableRows.push(<tr key={index}>
+                <td>{index}</td>
+                <td >{value.odk_unit_name}</td>
+                <td>{value.level}</td>
+                <td>{value.updated_at}</td>
+                <td>
+
+                    <a onClick={() => editOrg(value, setOrgToEdit)}
+                        href="#"
+                        style={{ "display": "inlineBlock", 'marginRight': '5px' }}
+                        className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                        <i className="fas fa-user-edit"></i>
+                    </a>
+                    <a onClick={() => deleteOrg(value.org_unit_id)} style={{ "display": "inlineBlock" }}
+                        className="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm">
+                        <i className="fas fa-user-times"></i>
+                    </a>
+                </td>
+            </tr>);
+        });
+    }
+    return tableRows;
+}
+
+
 function Orgunit() {
 
     const [showOrgunitLanding, setShowOrgunitLanding] = useState(true);
     const [tableOrgsStruct, setTableOrgsStruct] = useState();
+    const [orgToEdit, setOrgToEdit] = useState();
     (async () => {
         if (httpOrgUnits.length == 0) {
             httpOrgUnits = await FetchOrgunits();
@@ -58,7 +108,6 @@ function Orgunit() {
             setTableOrgsStruct(httpOrgUnits); ///save to state
         }
     })();
-
     const imgStyle = {
         width: "100%"
     };
@@ -67,36 +116,8 @@ function Orgunit() {
         marginBottom: "5px"
     };
 
-    const columns = [
-        {
-            name: "Org Unit Name",
-            selector: "odk_unit_name",
-            sortable: true
-        },
-        {
-            name: "Org Level",
-            selector: "level",
-            sortable: true
-        },
-        {
-            name: "Last updated",
-            selector: "updated_at",
-            sortable: true
-        }
-    ];
-   
     let pageContent = '';
-    let tableEl = <></>;
-    if (httpOrgUnits.length != 0) {
-        tableEl = <DataTable
-            title="Organisation Unit"
-            columns={columns}
-            data={httpOrgUnits.payload[0]}
-            defaultSortFieldId={1}
-            pagination
-            selectableRows
-        />
-    }
+    let tableEl = createOrgunitTable(httpOrgUnits, setOrgToEdit);
 
 
     if (showOrgunitLanding) {
@@ -111,9 +132,56 @@ function Orgunit() {
                     <TreeView orgUnits={tableOrgs} />
                 </div>
                 <div className="col-sm-9">
-                    {tableEl}
+                    <table className="table table-striped" >
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Org Unit Name</th>
+                                <th scope="col">Org Level</th>
+                                <th scope="col">Last Updated</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {tableEl}
+
+                        </tbody>
+                    </table>
                 </div>
             </div>
+
+            <div className="modal fade" id="editOrgModal" tabIndex="-1" role="dialog" aria-labelledby="editOrgModalTitle" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+
+                            <input id="" type="text"
+                                defaultValue={orgToEdit ? orgToEdit.odk_unit_name : ''}
+                                onChange={event => {
+                                    orgToEdit.odk_unit_name = event.target.value;
+                                }}
+                            />
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button"
+                                onClick={() => {
+                                    updateOrg(orgToEdit);
+                                    $('#editOrgModal').modal('toggle');
+                                }}
+                                className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
         </React.Fragment>
 
