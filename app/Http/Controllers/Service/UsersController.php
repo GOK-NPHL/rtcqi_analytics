db63bd67976235157e5efecaaad1511f7c9919be
duncan <duncanndiithi@yahoo.com>
2021-05-19 20:13:10 +0300

    retrieve db users

--- app/Http/Controllers/Service/UsersController.php
+++ app/Http/Controllers/Service/UsersController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Service;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -14,7 +16,7 @@ class UsersController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:sanctum');
     }
 
     /**
@@ -25,5 +27,22 @@ class UsersController extends Controller
     public function index()
     {
         return view('interface/users/index');
+    }
+
+    public function getUsers()
+    {
+        $user = Auth::user();
+        $users = User::select(
+            "users.name as first_name",
+            "users.last_name as last_name",
+            "users.email as email",
+            "roles.name as role_name",
+        )->join('roles', 'roles.id', '=', 'users.role_id')
+            ->where('users.id', '<>', $user->id)
+            ->get();
+
+        $roleIds = array();
+        $payload = array();
+        return  $users;
     }
 }
