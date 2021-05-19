45e3674a43d1ca0ccde5b8ba0d6c53bf112d5123
duncan <duncanndiithi@yahoo.com>
2021-05-19 11:25:11 +0300

    add orunits tree

--- resources/js/components/system/users/Users.js
+++ resources/js/components/system/users/Users.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import DropdownTreeSelect from 'react-dropdown-tree-select';
+import Register from './Register';
 
 class User extends React.Component {
 
@@ -125,59 +126,7 @@ class User extends React.Component {
                 </div>
             </div>;
         } else {
-            pageContent = <div id="registration_form" className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h6 className="m-0 font-weight-bold text-primary">Registration Form</h6>
-                </div>
-                <div className="card-body">
-
-                    <div className="card mb-4 py-3 border-left-secondary">
-                        <div className="card-body">
-                            <form className="needs-validation" novalidate>
-                                <div className="form-row">
-                                    <div className="col-md-6 mb-3">
-                                        <label for="validationTooltip01">First name</label>
-                                        <input type="text" className="form-control" id="validationTooltip01" value="Mark" required />
-                                        <div className="valid-tooltip">Looks good!</div>
-                                    </div>
-                                    <div className="col-md-6 mb-3">
-                                        <label for="validationTooltip02">Last name</label>
-                                        <input type="text" className="form-control" id="validationTooltip02" value="Otto" required />
-                                        <div className="valid-tooltip">Looks good!</div>
-                                    </div>
-                                </div>
-                                <div className="form-row">
-                                    <div className="col-md-6 mb-3">
-                                        <label for="validationTooltip03">Email</label>
-                                        <input type="text" className="form-control" id="validationTooltip03" required />
-                                        <div className="invalid-tooltip">Please provide a valid Email. </div>
-                                    </div>
-                                    <div className="col-md-6 mb-3">
-                                        <label for="validationTooltip05">Role</label>
-                                        <select className="form-control" id="exampleFormControlSelect1">
-                                            <option>National Manager</option>
-                                            <option>Implamenting Partner</option>
-                                            <option>County Medical Laboratory coordinators</option>
-                                            <option>Sub-County Medical Laboratory coordinators</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-
-                                <div className="form-row">
-                                    <div className="col-md-12 mb-12">
-                                        <label for="validationTooltip03">Organisation Units</label>
-                                        <div className="invalid-tooltip">Please provide a valid Email. </div>
-                                    </div>
-                                </div>
-
-                                <button className="btn btn-primary" type="submit">Save User</button>
-                            </form>
-                        </div>
-                    </div>
-
-                </div>
-            </div>;
+            pageContent = <Register/>;
         }
 
         return (
