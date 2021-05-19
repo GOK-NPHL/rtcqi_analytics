d0f1d69f06abe28530ab8f13c884b922241feb05
duncan <duncanndiithi@yahoo.com>
2021-05-19 18:48:37 +0300

    rename table

---
+++ database/migrations/2021_05_19_122306_create_user_organisationunit.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserOrganisationunit extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('odkorgunit_user', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer("odkorgunit_id");
+            $table->integer("user_id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('odkorgunit_user');
+    }
+}
