d2bf8381d0aba64bf149fb3c1b73f44ae2930b26
duncan <duncanndiithi@yahoo.com>
2021-05-21 11:03:22 +0300

    user delete controller

--- app/Http/Controllers/Service/UsersController.php
+++ app/Http/Controllers/Service/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Service;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -30,10 +31,11 @@ class UsersController extends Controller
     }
 
     public function getUsers()
-    {   
+    {
         $user = Auth::user();
         $users = User::select(
             "users.name as first_name",
+            "users.id as id",
             "users.last_name as last_name",
             "users.email as email",
             "roles.name as role_name",
@@ -44,5 +46,16 @@ class UsersController extends Controller
         $roleIds = array();
         $payload = array();
         return  $users;
+    }
+
+    public function deleteUser(Request $request)
+    {
+        try {
+            $user = User::find($request->user['id']);
+            $user->delete();
+            return response()->json(['Message' => 'Deleted successfully'], 200);
+        } catch (Exception $ex) {
+            return response()->json(['Message' => 'Delete failed.  Error code' . $ex->getMessage()], 500);
+        }
     }
 }
