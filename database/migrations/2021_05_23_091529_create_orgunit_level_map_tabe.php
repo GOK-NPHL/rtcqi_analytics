28360e2503d91371d6887c1df610b01678a00259
duncan <duncanndiithi@yahoo.com>
2021-05-23 12:23:07 +0300

    add org units level map tables

--- database/migrations/2021_05_23_091529_create_orgunit_level_map_tabe.php
+++ database/migrations/2021_05_23_091529_create_orgunit_level_map_tabe.php
@@ -4,9 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrgunitMetadataTabe extends Migration
+class CreateOrgunitLevelMapTabe extends Migration
 {
-    /**
+
+
+/**
      * Run the migrations.
      *
      * @return void
@@ -14,7 +16,7 @@ class CreateOrgunitMetadataTabe extends Migration
     public function up()
     {
 
-        Schema::create('orgunit_metadata', function (Blueprint $table) {
+        Schema::create('orgunit_level_maps', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string("sheet");
@@ -30,6 +32,7 @@ class CreateOrgunitMetadataTabe extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orgunit_metadata');
+        Schema::dropIfExists('orgunit_level_maps');
     }
+
 }
