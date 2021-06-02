6e8ed7301f3eb587f20172ddebf058db466f29c0
duncan <duncanndiithi@yahoo.com>
2021-06-02 22:09:34 +0300

    display multiple charts

--- resources/js/components/reports/spi/BarColumnCharts.js
+++ resources/js/components/reports/spi/BarColumnCharts.js
@@ -20,158 +20,136 @@ class BarColumnCharts extends React.Component {
                         focus: 'series'
                     },
                     data: [24, 23, 56, 34, 32]
-                },
-                {
-                    name: 'Level 1  (50-59%)',
-                    type: 'bar',
-                    stack: 'total',
-                    label: {
-                        show: true
-                    },
-                    emphasis: {
-                        focus: 'series'
-                    },
-                    data: [55, 67, 22, 76, 67]
-                },
-                {
-                    name: 'Level 2 (60-79%)',
-                    type: 'bar',
-                    stack: 'total',
-                    label: {
-                        show: true
-                    },
-                    emphasis: {
-                        focus: 'series'
-                    },
-                    data: [66, 32, 56, 87, 32]
-                },
-                {
-                    name: 'Level 3 (80-89%)',
-                    type: 'bar',
-                    stack: 'total',
-                    label: {
-                        show: true
-                    },
-                    emphasis: {
-                        focus: 'series'
-                    },
-                    data: [78, 11, 34, 75, 23]
-                },
-                {
-                    name: 'Level 4 (>90%)',
-                    type: 'bar',
-                    stack: 'total',
-                    label: {
-                        show: true
-                    },
-                    emphasis: {
-                        focus: 'series'
-                    },
-                    data: [10, 45, 56, 76, 32]
                 }
             ]
         };
+        this.createOverallSiteGraphsDisplays = this.createOverallSiteGraphsDisplays.bind(this);
+        this.prepareOverallLevelSiteData = this.prepareOverallLevelSiteData.bind(this);
+
     }
 
     componentDidMount() {
 
     }
 
+    prepareOverallLevelSiteData(dataObject) {
+        let overallSiteGraphsData = {};
+        let levelsMap = {
+            'level0': 'Level 0 (<40%)',
+            'level1': 'Level 1 (50-59%)',
+            'level2': 'Level 2 (60-79%)',
+            'level3': 'Level 3 (80-89%)',
+            'level4': 'Level 4 (>90%)'
+        }
+
+
+        let orgName = dataObject['orgName'];
+        if (dataObject['OrgUniType']) orgName += ' ' + dataObject['OrgUniType'];
+
+        let overallSitesObject = dataObject['OverallSitesLevel'];
+        let levelData = { 'level0': [], 'level1': [], 'level2': [], 'level3': [], 'level4': [] };
+        let category = [];
+        let letSeriesData = [];
+
+        for (let [timeline, timeLineObjectValue] of Object.entries(overallSitesObject)) {
+            if (!category.includes(timeline)) {
+                category.push(timeline);
+            }
+
+            for (let [levelName, levelValue] of Object.entries(timeLineObjectValue)) {
+
+                if (levelName != 'counter') {
+                    levelData[levelName].push(levelValue);
+                }
+
+            }
+        }
+        for (let [level, dataArray] of Object.entries(levelData)) {
+            let seriesEntry = {
+                name: '',
+                type: 'bar',
+                stack: 'total',
+                label: {
+                    show: true
+                },
+                emphasis: {
+                    focus: 'series'
+                },
+                data: ''
+            };
+            seriesEntry['data'] = dataArray;
+            seriesEntry['name'] = levelsMap[level];
+            letSeriesData.push(seriesEntry);
+        }
+        overallSiteGraphsData[orgName] = [category, letSeriesData];
+
+        //prepare percentage of sites assessed graph data
+
+        // return this.createOverallSiteGraphsDisplays(overallSiteGraphsData, counter);
+        return <RTCard header={orgName}>
+            <StackedHorizontal category={category} series={letSeriesData} />
+        </RTCard>
+    }
+
+    createOverallSiteGraphsDisplays(overallSiteGraphsData, counter) {
+
+    }
+
     render() {
         let overLay = [];
-        let counter = 1;
+        let counter = 0;
         let columns = [];
         let row = <div key={uuidv4()} className="row">
             {columns}
-        </div>
-
-
+        </div>;
         if (this.props.serverData) {
-            console.log(" no tempty");
-            console.log(this.props.serverData);
-            let overallSiteGraphsData = {};
             if (Array.isArray(this.props.serverData)) {
-                let levelsMap = {
-                    'level0': 'Level 0 (<40%)',
-                    'level1': 'Level 1 (50-59%)',
-                    'level2': 'Level 2 (60-79%)',
-                    'level3': 'Level 3 (80-89%)',
-                    'level4': 'Level 4 (>90%)'
-                }
+                console.log("last");
+                console.log(this.props.serverData);
 
                 this.props.serverData.map((dataObject) => {
-
-                    let orgName = dataObject['orgName'];
-                    if (dataObject['OrgUniType']) orgName += ' ' + dataObject['OrgUniType'];
-
-                    let overallSitesObject = dataObject['OverallSitesLevel'];
-                    let levelData = { 'level0': [], 'level1': [], 'level2': [], 'level3': [], 'level4': [] };
-                    let category = [];
-                    let letSeriesData = [];
-
-                    for (let [timeline, timeLineObjectValue] of Object.entries(overallSitesObject)) {
-                        if (!category.includes(timeline)) {
-                            category.push(timeline);
-                        }
-
-                        for (let [levelName, levelValue] of Object.entries(timeLineObjectValue)) {
-
-                            if (levelName != 'counter') {
-                                levelData[levelName].push(levelValue);
-                            }
-
-                        }
+                    let singChart = this.prepareOverallLevelSiteData(dataObject);
+                    if (counter % 2 == 0) {
+                        overLay.push(row);
+                        columns = [];
+                        row = <div key={uuidv4()} className="row">
+                            {columns}
+                        </div>;
                     }
-                    for (let [level, dataArray] of Object.entries(levelData)) {
-                        let seriesEntry = {
-                            name: '',
-                            type: 'bar',
-                            stack: 'total',
-                            label: {
-                                show: true
-                            },
-                            emphasis: {
-                                focus: 'series'
-                            },
-                            data: ''
-                        };
-                        seriesEntry['data'] = dataArray;
-                        seriesEntry['name'] = levelsMap[level];
-                        letSeriesData.push(seriesEntry);
-                    }
-                    overallSiteGraphsData[orgName] = [category, letSeriesData];
-                    //prepare percentage of sites assessed graph data
-                });
-
-            }
-
-
-            for (let [headerName, graphData] of Object.entries(overallSiteGraphsData)) {
-
-                if (counter % 2 == 0) {
-
-                    overLay.push(row);
-                    columns = [];
-                    row = <div key={uuidv4()} className="row">[columns]</div>;
-                } else {
-
                     columns.push(<div key={uuidv4()} className="col-sm-6 col-xm-12">
-                        <RTCard header={headerName}>
-                            <StackedHorizontal category={graphData[0]} series={graphData[1]} />
-                        </RTCard>
+                        {singChart}
                     </div>);
+                    counter += 1;
+                });
+                if (columns.length > 0) {
+                    overLay.push(row); //push remaining graphs in display
                 }
-                counter += 1;
 
-            }
-            if (columns.length > 0) { //push remaining elements on the array
+            } else {
+                console.log("last 1");
+                console.log(this.props.serverData);
+                for (let [key, dataObject] of Object.entries(this.props.serverData)) {
+                    let singChart = this.prepareOverallLevelSiteData(dataObject);
 
-                overLay.push(row);
+                    if (counter % 2 == 0) {
+                        overLay.push(row);
+                        columns = [];
+                        row = <div key={uuidv4()} className="row">
+                            {columns}
+                        </div>;
+                    }
+                    columns.push(<div key={uuidv4()} className="col-sm-6 col-xm-12">
+                        {singChart}
+                    </div>);
+                    counter += 1;
+                }
+                if (columns.length > 0) {
+                    overLay.push(row); //push remaining graphs in display
+                }
             }
-            console.log("overLay");
-            console.log(overLay);
+
         } else {
-            console.log(" empty");
+            console.log("empty");
         }
 
         return (
