dbebe88ea79103861aa7fbb6a6221394dc28711d
duncan <duncanndiithi@yahoo.com>
2021-06-03 12:32:39 +0300

    expire orgs data on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,7 +53,9 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" onclick="localStorage.removeItem('orgunitList');    
+                                                localStorage.removeItem('treeStruc');"
+                                 class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
