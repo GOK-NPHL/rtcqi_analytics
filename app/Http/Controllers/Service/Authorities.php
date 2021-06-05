38e6a3e27847db7cb5d83a7681f43d7b2840f6ae
duncan <duncanndiithi@yahoo.com>
2021-06-05 16:01:57 +0300

    return null org if nor rlaship

--- app/Http/Controllers/Service/Authorities.php
+++ app/Http/Controllers/Service/Authorities.php
@@ -30,12 +30,16 @@ class Authorities extends Controller
     {
         $user = Auth::user();
         $roles = Authority::select(
-            "authorities.name "
+            "authorities.name"
         )->join('authority_role', 'authorities.id', '=', 'authority_role.authority_id')
             ->join('roles', 'roles.id', '=', 'authority_role.role_id')
-            ->join('users', 'role_id', '=', 'roles.id')
+            ->join('users', 'users.role_id', '=', 'roles.id')
             ->where('users.id', $user->id)
             ->get();
-        return $roles;
+        if (count($roles) > 0) {
+            return $roles;
+        } else {
+            return [];
+        }
     }
 }
