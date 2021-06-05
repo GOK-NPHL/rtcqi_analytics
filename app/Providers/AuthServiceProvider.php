cc0543943882bec981eba4eee4bd13b215f4b4de
duncan <duncanndiithi@yahoo.com>
2021-06-05 13:50:14 +0300

    auth authorities gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Services\SystemAuthorities;
+use App\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,22 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define(SystemAuthorities::$authorities['view_orgunit'], function ($user) {
+            $user = User::select(
+                "users.id as id"
+            )->join('roles', 'roles.id', '=', 'users.role_id')
+                ->join('authority_role', 'roles.id', '=', 'authority_role.role_id')
+                ->join('authorities', 'authorities.id', '=', 'authority_role.authority_id')
+                ->where('authorities.name', SystemAuthorities::$authorities['view_orgunit'])
+                ->get();
+            Log::info("the user got");
+            Log::info($user);
+
+            if (count($user) != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
 }
