5c87152b3838781df99fa941c5c4f343d88e622c
duncan <duncanndiithi@yahoo.com>
2021-06-07 15:48:11 +0300

    menu access permissions

--- resources/views/layouts/rtcqi.blade.php
+++ resources/views/layouts/rtcqi.blade.php
@@ -1,6 +1,11 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
+<?php
+
+use Illuminate\Support\Facades\Gate;
+?>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -42,45 +47,59 @@
                 <div style="margin-left: 0px !important" class="sidebar-brand-text mx-3">RT-CQI Analytics</div>
             </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('home') }}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+            <?php if (Gate::allows('view_dashboard')) { ?>
+                <!-- Divider -->
+                <hr class="sidebar-divider my-0">
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+                <!-- Nav Item - Dashboard -->
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">
+                        <i class="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Dashboard</span></a>
+                </li>
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Interface
-            </div>
+            <?php } ?>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Reports</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Reports:</h6>
-                        <a class="collapse-item" href="{{ route('ptIndex') }}">Pt</a>
-                        <a class="collapse-item" href="{{ route('logbookIndex') }}">Log book</a>
-                        <a class="collapse-item" href="{{ route('spiIndex') }}">SPI-RT</a>
-                        <!-- <a class="collapse-item" href="{{ route('meIndex') }}">M&E</a>
-                            <a class="collapse-item" href="{{ route('summariesIndex') }}">Summaries</a> -->
-                    </div>
-                </div>
-            </li>
-
+            <!--Reports section  -->
             <?php
 
-            use Illuminate\Support\Facades\Gate;
+            if (Gate::allows('view_reports')) { ?>
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Reports
+                </div>
+
+                <!-- Nav Item - Pages Collapse Menu -->
+                <li class="nav-item">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                        <i class="fas fa-fw fa-folder"></i>
+                        <span>Reports</span>
+                    </a>
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                        <div class="bg-white py-2 collapse-inner rounded">
+                            <h6 class="collapse-header">Reports:</h6>
+                            <?php if (Gate::allows('view_pt_report')) { ?>
+                                <a class="collapse-item" href="{{ route('ptIndex') }}">Pt</a>
+                            <?php } ?>
+                            <?php if (Gate::allows('view_log_book_report')) { ?>
+                                <a class="collapse-item" href="{{ route('logbookIndex') }}">Log book</a>
+                            <?php } ?>
+                            <?php if (Gate::allows('view_spi_report')) { ?>
+                                <a class="collapse-item" href="{{ route('spiIndex') }}">SPI-RT</a>
+                            <?php } ?>
+                            <!-- <a class="collapse-item" href="{{ route('meIndex') }}">M&E</a>
+                            <a class="collapse-item" href="{{ route('summariesIndex') }}">Summaries</a> -->
+                        </div>
+                    </div>
+                </li>
+            <?php } ?>
+
+             <!--System admin section  -->
+            <?php
 
             if (Gate::allows('view_system_settings')) { ?>
                 <!-- Divider -->
