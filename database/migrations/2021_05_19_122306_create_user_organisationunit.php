1ba60737f489fe16292c680b84f6bcb36a02eff3
duncan <duncanndiithi@yahoo.com>
2021-06-08 21:21:26 +0300

    fix datatype mismatch

--- database/migrations/2021_05_19_122306_create_user_organisationunit.php
+++ database/migrations/2021_05_19_122306_create_user_organisationunit.php
@@ -16,7 +16,7 @@ class CreateUserOrganisationunit extends Migration
         Schema::create('odkorgunit_user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->integer("odk_orgunit_id");
+            $table->uuid("odk_orgunit_id");
             $table->integer("user_id");
         });
     }
