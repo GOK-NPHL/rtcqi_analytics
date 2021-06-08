4e07628b6c88fd6cd83eb9fd741f16f9952efc66
duncan <duncanndiithi@yahoo.com>
2021-06-08 21:22:04 +0300

    limit org units returned

--- app/Http/Controllers/Service/OrgunitsController.php
+++ app/Http/Controllers/Service/OrgunitsController.php
@@ -35,22 +35,137 @@ class OrgunitsController extends Controller
         return view('interface/orgunits/index');
     }
 
+    private function runOrgUnitsLevelQuery($orgUnitId, $level)
+    {
+        if ($level == 5) {
+            //site
+            $orgUnitObject = OdkOrgunit::select(
+                "odkorgunit.level",
+                "odkorgunit.odk_unit_name",
+                "odkorgunit.parent_id",
+                "odkorgunit.org_unit_id"
+            )->where('odkorgunit.org_unit_id', $orgUnitId)
+                ->get();
+
+            return $orgUnitObject;
+        } else if ($level == 4) {
+            //facility
+            $orgUnitObject = OdkOrgunit::select(
+                "odkorgunit.level",
+                "odkorgunit.odk_unit_name",
+                "odkorgunit.parent_id",
+                "odkorgunit.org_unit_id",
+                "org5.level",
+                "org5.odk_unit_name",
+                "org5.parent_id",
+                "org5.org_unit_id"
+            )->join('odkorgunit as org5', 'org5.parent_id', '=', 'odkorgunit.org_unit_id')
+                ->where('odkorgunit.org_unit_id', $orgUnitId)
+                ->get();
+
+            return $orgUnitObject;
+        } else if ($level == 3) {
+            //sub county
+            $orgUnitObject = OdkOrgunit::select(
+                "odkorgunit.level",
+                "odkorgunit.odk_unit_name",
+                "odkorgunit.parent_id",
+                "odkorgunit.org_unit_id",
+                "org4.level",
+                "org4.odk_unit_name",
+                "org4.parent_id",
+                "org4.org_unit_id",
+                "org5.level",
+                "org5.odk_unit_name",
+                "org5.parent_id",
+                "org5.org_unit_id"
+            )->join('odkorgunit as org4', 'org4.parent_id', '=', ' odkorgunit.org_unit_id')
+                ->join('odkorgunit as org5', 'org5.parent_id', '=', 'org4.org_unit_id')
+                ->where('odkorgunit.org_unit_id', $orgUnitId)
+                ->get();
+            return $orgUnitObject;
+        } else if ($level == 2) {
+            //county
+            $orgUnitObject = OdkOrgunit::select(
+                "odkorgunit.level",
+                "odkorgunit.odk_unit_name",
+                "odkorgunit.parent_id",
+                "odkorgunit.org_unit_id",
+                "org4.level",
+                "org4.odk_unit_name",
+                "org4.parent_id",
+                "org4.org_unit_id",
+                "org5.level",
+                "org5.odk_unit_name",
+                "org5.parent_id",
+                "org5.org_unit_id",
+                "org3.level",
+                "org3.odk_unit_name",
+                "org3.parent_id",
+                "org3.org_unit_id",
+            )->join('odkorgunit as org3', 'org3.parent_id', '=', 'odkorgunit.org_unit_id')
+                ->join('odkorgunit as org4', 'org4.parent_id', '=', ' org3.org_unit_id')
+                ->join('odkorgunit as org5', 'org5.parent_id', '=', 'org4.org_unit_id')
+                ->where('odkorgunit.org_unit_id', $orgUnitId)
+                ->get();
+
+            return $orgUnitObject;
+        } else if ($level == 1) {
+            //national
+            $orgUnitObject = OdkOrgunit::select(
+                "odkorgunit.level",
+                "odkorgunit.odk_unit_name",
+                "odkorgunit.parent_id",
+                "odkorgunit.org_unit_id"
+            )->get();
+
+            return $orgUnitObject;
+        }
+    }
+
+    private function getOrgunitsByUser()
+    {
+        // get all org units & their levels registered.
+        // for each orgunit registered, get its childeren upto lowest level.
+        $combinedRecords = [];
+        //$combinedRecords = array_merge($combinedRecords, iterator_to_array($perCountyRecords, true));
+        $user = Auth::user();
+
+        $registeredOrgs = OdkOrgunit::select(
+            "odkorgunit.level",
+            "odkorgunit.org_unit_id"
+        )->join('odkorgunit_user', 'odkorgunit_user.odk_orgunit_id', '=', 'odkorgunit.org_unit_id')
+            ->join('users', 'users.id', '=', 'odkorgunit_user.user_id')
+            ->where('users.id', $user->id)
+            ->get();
+        foreach ($registeredOrgs as $registeredOrg) {
+
+            $orgUnitResultSet = $this->runOrgUnitsLevelQuery($registeredOrg->org_unit_id, $registeredOrg->level);
+            $combinedRecords = array_merge($combinedRecords, iterator_to_array($orgUnitResultSet, true));
+        }
+        return  $combinedRecords;
+    }
+
+
     public function getOrgunits()
     {
         if (!Gate::allows(SystemAuthorities::$authorities['view_orgunit'])) {
             return response()->json(['Message' => 'Not allowed to view organisation units: '], 500);
         }
-        $levels = OdkOrgunit::select("level")->orderBy('level', 'asc')->groupByRaw('level')->get();
-        $levelsArr = array();
-        foreach ($levels as $level) {
-            $levelsArr[] = $level->level;
-        }
-        $orgUnitsPayload = [
-            'metadata' =>
-            ['levels' => $levelsArr],
-            'payload' => [OdkOrgunit::all()]
-        ];
-        return $orgUnitsPayload;
+
+        return $this->getOrgunitsByUser();
+
+        // $levels = OdkOrgunit::select("level")->orderBy('level', 'asc')->groupByRaw('level')->get();
+        // $levelsArr = array();
+        // foreach ($levels as $level) {
+        //     $levelsArr[] = $level->level;
+        // }
+        // $orgUnitsPayload = [
+        //     'metadata' =>
+        //     ['levels' => $levelsArr],
+        //     'payload' => [OdkOrgunit::all()]
+        // ];
+        // return $orgUnitsPayload;
     }
 
     public function saveOrgunits(Request $request)
