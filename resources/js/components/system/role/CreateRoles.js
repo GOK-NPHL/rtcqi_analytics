468d5f25a2a22a7a01415187f1235b7f78357efa
duncan <duncanndiithi@yahoo.com>
2021-06-08 11:20:02 +0300

    athorize role edit/add

--- resources/js/components/system/role/CreateRoles.js
+++ resources/js/components/system/role/CreateRoles.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FetchAuthorities, SaveRole, UpdateRole } from '../../utils/Helpers';
+import { FetchAuthorities, SaveRole, UpdateRole, FetchUserAuthorities } from '../../utils/Helpers';
 import DualListBox from 'react-dual-listbox';
 
 
@@ -12,14 +12,15 @@ class RoleCreate extends React.Component {
         this.state = {
             selected: [],
             roleName: '',
-            permissionOptions: []
+            permissionOptions: [],
+            allowedPermissions: []
         };
-
         this.saveRole = this.saveRole.bind(this);
         this.authoritiesOnChange = this.authoritiesOnChange.bind(this);
     }
 
     componentDidMount() {
+
         (async () => {
             let returnedData = await FetchAuthorities(); //authorities used for creation of new roles
             let allowedPermissions = await FetchUserAuthorities(); //authorities logged in ausers has been given on the system.
@@ -50,7 +51,6 @@ class RoleCreate extends React.Component {
         })();
 
         if (this.props.editMode) {
-            console.log(this.props.roleToEdit);
             this.setState({ roleName: this.props.roleToEdit.role_name });
             let currentAuthorities = this.props.roleToEdit.authorities;
             let selected = [];
@@ -60,7 +60,6 @@ class RoleCreate extends React.Component {
                 }
             }
 
-            console.log(selected);
             this.setState({ selected: selected });
         }
 
@@ -93,49 +92,55 @@ class RoleCreate extends React.Component {
                 }
             }
         }
-        this.props.updateEditMode(false);
+        //   this.props.updateEditMode(false);
     }
 
-
-
     render() {
+        let pageContent = <div id="registration_form" className="card shadow mb-4">
+            <div className="card-header py-3">
+                <h6 className="m-0 font-weight-bold text-primary">Role Creation</h6>
+            </div>
+            <div className="card-body">
+
+                <div className="card mb-4 py-3 border-left-secondary">
+                    <div className="card-body">
+                        <form className="needs-validation" noValidate>
+                            <div className="form-row">
+                                <div className="col-md-12 mb-3">
+                                    <label htmlFor="role_name">Role name</label>
+                                    <input type="text" onChange={event => this.setState({ roleName: event.target.value })}
+                                        value={this.state.roleName} className="form-control" id="role_name" required />
+                                    <div className="valid-tooltip">Role name</div>
+                                </div>
+                                <div className="col-md-12 mb-3">
+                                    <label htmlFor="permissions">Assign permissions</label>
+                                    <DualListBox
+                                        canFilter
+                                        options={this.state.permissionOptions}
+                                        selected={this.state.selected}
+                                        onChange={this.authoritiesOnChange}
+                                    />
+                                </div>
+                            </div>
+                            <button onClick={this.saveRole} className="btn btn-primary" type="submit">Save Role</button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>;
+
+        if (this.state.allowedPermissions.length == 0) {
+            pageContent = '';
+        } else if (this.props.editMode && !this.state.allowedPermissions.includes('edit_role')) {
+            pageContent = '';
+        } else if (!this.props.editMode && !this.state.allowedPermissions.includes('add_role')) {
+            pageContent = '';
+        }
 
         return (
             <React.Fragment>
-
-                <div id="registration_form" className="card shadow mb-4">
-                    <div className="card-header py-3">
-                        <h6 className="m-0 font-weight-bold text-primary">Role Creation</h6>
-                    </div>
-                    <div className="card-body">
-
-                        <div className="card mb-4 py-3 border-left-secondary">
-                            <div className="card-body">
-                                <form className="needs-validation" noValidate>
-                                    <div className="form-row">
-                                        <div className="col-md-12 mb-3">
-                                            <label htmlFor="role_name">Role name</label>
-                                            <input type="text" onChange={event => this.setState({ roleName: event.target.value })}
-                                                value={this.state.roleName} className="form-control" id="role_name" required />
-                                            <div className="valid-tooltip">Role name</div>
-                                        </div>
-                                        <div className="col-md-12 mb-3">
-                                            <label htmlFor="permissions">Assign permissions</label>
-                                            <DualListBox
-                                                canFilter
-                                                options={this.state.permissionOptions}
-                                                selected={this.state.selected}
-                                                onChange={this.authoritiesOnChange}
-                                            />
-                                        </div>
-                                    </div>
-                                    <button onClick={this.saveRole} className="btn btn-primary" type="submit">Save Role</button>
-                                </form>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+                {pageContent}
             </React.Fragment>
         );
     }
