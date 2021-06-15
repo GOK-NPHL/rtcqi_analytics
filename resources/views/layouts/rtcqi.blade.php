dc5e768e23afcc2506e2067a0e7260505a645240
duncan <duncanndiithi@yahoo.com>
2021-06-15 14:46:39 +0300

    enable profile page routting

--- resources/views/layouts/rtcqi.blade.php
+++ resources/views/layouts/rtcqi.blade.php
@@ -263,9 +263,9 @@ use Illuminate\Support\Facades\Gate;
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="{{ route('profile') }}">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
+                                    My Profile
                                 </a>
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
