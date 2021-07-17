2913c0c53f81e5be0a9751c9c0f7ff0fb5d09e0d
duncan <duncanndiithi@yahoo.com>
2021-07-17 17:06:03 +0300

    hts data controller

--- app/Http/Controllers/LogbookReportController.php
+++ app/Http/Controllers/LogbookReportController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\SystemAuthorities;
 use Illuminate\Http\Request;
+use App\Services\ODkHTSDataAggregator;
+use App\Services\SystemAuthorities;
+use Exception;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 
 class LogbookReportController extends Controller
 {
@@ -29,5 +32,24 @@ class LogbookReportController extends Controller
             return response()->json(['Message' => 'Not allowed to view log book report: '], 500);
         }
         return view('reports/logbook/index');
+    }
+
+    public function getData(Request $request)
+    {
+        if (!Gate::allows(SystemAuthorities::$authorities['view_log_book_report'])) {
+            return response()->json(['Message' => 'Not allowed to view log book report: '], 500);
+        }
+        try {
+            $odkObj = new ODkHTSDataAggregator;
+            $orgUnitIds = $request->orgUnitIds;
+            $siteType = $request->siteType;
+            $startDate = $request->startDate;
+            $endDate = $request->endDate;
+
+            $result = $odkObj->getData($orgUnitIds, $siteType, $startDate, $endDate);
+            return $result;
+        } catch (Exception $ex) {
+            return response()->json(['Message' => 'Could not fetch data: ' . $ex->getMessage()], 500);
+        }
     }
 }
