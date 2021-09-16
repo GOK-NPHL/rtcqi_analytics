d9808329c0081636592d1504438228fbdb3323dd
duncan <duncanndiithi@yahoo.com>
2021-09-16 12:05:52 +0300

    contextual heading

--- resources/js/components/reports/spi/SpiReport.js
+++ resources/js/components/reports/spi/SpiReport.js
@@ -445,7 +445,10 @@ class SpiReport extends React.Component {
 
                 {/* Page Heading */}
                 <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 className="h4 mb-0 text-gray-500">SPI REPORT</h1>
+                    <h1 className="h4 mb-0 text-gray-500">SPI REPORT: {
+                        this.state.orgUnitIndicators[this.state.indicatorIndexToDisplay]
+                    }
+                    </h1>
                     {/* <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                         className="fas fa-download fa-sm text-white-50"></i> Generate Report</a> */}
                 </div>
