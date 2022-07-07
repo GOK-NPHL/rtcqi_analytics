fa6176356a3cfdab70eef928b7a38ae990dd0c8e
Benzer <benzerbett@gmail.com>
2022-07-07 12:33:49 +0300

    resourcefiles model

--- app/ResourceFiles.php
+++ app/ResourceFiles.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class ResourceFiles extends Model
 {
-    //
+    protected $table = 'resource_files';
+
+    protected $fillable = ['name', 'path', 'type', 'size', 'is_public'];
+
+    public function getMimeType(){
+        return mime_content_type($this->path);
+    }
+
+    public function getSize(){
+        return filesize($this->path);
+    }
+
+    public function getUrl(){
+        return url('/files/'.$this->name);
+    }
+
+    // delete file when deleting resource file
+    public function delete(){
+        unlink($this->path);
+        parent::delete();
+    }
 }
