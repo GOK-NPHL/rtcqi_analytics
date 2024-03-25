cf720ce0b597907fdc28ff6f52b3acd2363433ad
Bett <benzerbett@gmail.com>
2024-03-25 15:50:20 +0300

    Update stateful domains in sanctum.php config file

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,localhost:8092,127.0.0.1,127.0.0.1:8092,::1,'.parse_url(env('APP_URL'), PHP_URL_HOST)
+        'localhost,localhost:8092,127.0.0.1,127.0.0.1:8092,0.0.0.0:8092,rtcqi.nphl.go.ke,::1,'.parse_url(env('APP_URL'), PHP_URL_HOST)
     )),
 
     /*
