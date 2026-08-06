3580f215be0f25d473bf014df0b1e1321bfe0db3
benzerbett <benzerbett@gmail.com>
2026-08-06 15:15:36 +0300

    Followup 'expansion' 10 -> 16

--- app/Services/ODKDataAggregator.php
+++ app/Services/ODKDataAggregator.php
@@ -22,7 +22,7 @@ use PhpParser\Node\Stmt\Continue_;
 class ODKDataAggregator
 {
     private $reportSections = array();
-    private $timeLines = ['baseline', 'follow1', 'follow2', 'follow3', 'follow4', 'follow5', 'follow6', 'follow7', 'follow8', 'follow9', 'follow10', 'other'];
+    private $timeLines = ['baseline', 'follow1', 'follow2', 'follow3', 'follow4', 'follow5', 'follow6', 'follow7', 'follow8', 'follow9', 'follow10', 'follow11', 'follow12', 'follow13', 'follow14', 'follow15', 'follow16', 'other'];
     private $userOrgTimelineParams = array();
     private $siteType = null;
     private $startDate = null;
